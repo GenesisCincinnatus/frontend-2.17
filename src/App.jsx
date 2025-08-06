f47c2c482ef6b956c09d4bc17dfe3ae7b08a7d46
Génesis Milagros Ramos Padilla <gmrpmarshmallow03@gmail.com>
2025-08-06 17:59:06 -0400

    Actualiza App.jsx y lógica

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,11 @@ import personService from './services/persons'
 const Notification = ({ message, type }) => {
   if (!message) return null
 
-  const style = {
-    color: type === 'error' ? 'red' : 'green',
-    background: '#f2f2f2',
-    fontSize: 16,
-    border: `2px solid ${type === 'error' ? 'red' : 'green'}`,
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  }
-
-  return <div style={style}>{message}</div>
+  return (
+    <div className={type === 'error' ? 'error-message' : 'notification'}>
+      {message}
+    </div>
+  )
 }
 
 const App = () => {
